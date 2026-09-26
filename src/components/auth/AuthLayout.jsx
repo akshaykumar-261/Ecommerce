c3cbd04d57420fa8506065e3fb9c74618f19faab
Akshay Kumar~ <akshaykumar261@gmail.com>
2026-09-26 17:06:35 +0530

    Sep 26 Change 11

--- src/components/auth/AuthLayout.jsx
+++ src/components/auth/AuthLayout.jsx
@@ -16,11 +16,11 @@ function AuthLayout({ children, image }) {
         "
       >
         {/* LEFT SIDE - IMAGE */}
-        <div className="relative min-h-0 overflow-hidden bg-violet-100 hidden md:block">
+        <div className="relative min-h-0 overflow-hidden bg-gray-100 hidden md:block">
           <img
             src={image}
             alt="ShopEase"
-            className="absolute inset-0 block h-full w-full object-cover"
+            className="absolute inset-0 block h-full w-full object-center object-center"
           />
         </div>
 
